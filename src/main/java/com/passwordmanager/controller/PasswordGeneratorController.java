8f558ed0298c7f5dfa3ff17dcb84ec29a66aa742
soumya0715 <soumya20901@gmail.com>
2026-02-24 01:33:42 +0530

    Updated PasswordGenerator Controller

--- src/main/java/com/passwordmanager/controller/PasswordGeneratorController.java
+++ src/main/java/com/passwordmanager/controller/PasswordGeneratorController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/generator")
+@RequestMapping("/Password-generator")
 @RequiredArgsConstructor
 public class PasswordGeneratorController {
 
